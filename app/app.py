8114d84eb7e2ad23b4ba044f6a12211727767d6c
Atomicbeast101 <adam@brodack.net>
2023-09-18 21:18:27 -0500

    Fixed payload conditions

--- app/app.py
+++ app/app.py
@@ -81,8 +81,8 @@ def on_message(client, userdata, msg):
     log.debug('Topic received: {topic}'.format(topic=msg.topic))
     if msg.topic == PROJECTOR_MQTT_TOPIC.format(name=config.PROJECTOR_NAME.lower(), path='projector/set'):
         log.info('Received toggle command from HomeAssistant...')
-        log.debug('Payload received: {}'.format(msg.payload))
-        if msg.payload == 'ON':
+        log.debug('Payload received: {}'.format(msg.payload.decode().upper()))
+        if msg.payload.decode().upper() == 'ON':
             success, reason = proj.on()
             if success:
                 log.info('Successfully turned on the projector!')
@@ -92,7 +92,7 @@ def on_message(client, userdata, msg):
                 elif reason == 'bad_data':
                     log.error('Unexpected output from the projector! This is the output it received: {}'.format(reason['data']))
 
-        elif msg.payload == 'OFF':
+        elif msg.payload.decode().upper() == 'OFF':
             success, reason = proj.off()
             if success:
                 log.info('Successfully turned off the projector!')
